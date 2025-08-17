8015786aa29eeea45d0fc0a8d3cf89388c54eb46
Madhav P <madhavp2023@gmail.com>
2025-08-17 17:29:42 +0530

    Level-4 Completed

--- jeevansetu-backend/src/middleware/authMiddleware.js
+++ jeevansetu-backend/src/middleware/authMiddleware.js
@@ -1,1 +1,28 @@
 // jeevansetu-backend/src/middleware/authMiddleware.js
+
+import jwt from "jsonwebtoken";
+
+// Auth Middleware
+export const protect = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization || "";
+
+    const token = authHeader.startsWith("Bearer ")
+      ? authHeader.substring(7)
+      : null;
+
+    if (!token) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    res.user = payload;
+    req.userId = payload.id;
+    req.user = { id: payload.id };
+    return next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid or expired token" });
+  }
+};
--- jeevansetu-backend/src/routes/authRoutes.js
+++ jeevansetu-backend/src/routes/authRoutes.js
@@ -4,6 +4,8 @@ import express from "express";
 
 import { registerUser, loginUser } from "../controllers/authController.js";
 
+import { protect } from "../middleware/authMiddleware.js";
+
 
 // Initialize router
 const router = express.Router();
@@ -12,5 +14,12 @@ const router = express.Router();
 // Routes 
 router.post('/register' ,  registerUser);
 router.post('/login' , loginUser);
+router.get('/protected', protect , (req ,res) =>{
+    return res.status(200).json({
+        success : true,
+        message : "Protected route accessed Successfully",
+        userId  : req.userId
+    })
+})
 
 export default router;
